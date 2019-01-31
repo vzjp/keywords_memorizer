d0978f16e09c5b70a63b1db8e6405175ad721e30
vzjp <23609056+vzjp@users.noreply.github.com>
2019-01-31 12:45:43 +0900

    Able to choose md file via index page link.

--- markdownFlask.py
+++ markdownFlask.py
@@ -1,10 +1,12 @@
 # We import the markdown library
 import markdown
+import urllib.parse
 from flask import Flask
 from flask import render_template
 from flask import Markup
 import sys
 from fileSelector import file_selector
+from fileSelector import ls_md_files
 
 app = Flask(__name__)
 
@@ -12,30 +14,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	dirname = './markdown-files/'
-	filename = file_selector(dirname)
-	content = ''
-	fi = open(filename, encoding='utf-8')
-	line = fi.readline()
-	while line:
-		content += line
-		line = fi.readline()
-	fi.close()
-	md = markdown.Markdown(extensions=['markdown.extensions.tables'])
-	content = Markup(md.convert(content))
-	return render_template('template_markdown.html', **locals())
-	'''
-	md_file_list = ls_md_files('./markdown-files/')
-	links = ''
-	for i in range(0, md_file_list):
-		link = '<a href="./{}">{}</a>'.format(i, i)
-		links = links + link
-	return render_template('index.html', **locals())
-	'''
+	files = ls_md_files(dirname)
+	content = '<ul>'
+	for a_file in files:
+		content += "<li><a href=./{}>{}</a></li>\n".format(a_file, a_file)
+	content += "</ul>"
+	content = Markup(content)
+	return render_template('index.html', content=content)
+
 
 @app.route('/<filename>')
 def anki(filename):
 	dirname = './markdown-files/'
-	filename = dirname + filename
+	filename = dirname + urllib.parse.unquote(filename)
 	content = ''
 	fi = open(filename, encoding='utf-8')
 	line = fi.readline()
